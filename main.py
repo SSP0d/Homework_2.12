b7473fef5059ce92f60e274a6069fa4f9a945228
Serhii Pidkopai <spodkopay@gmail.com>
2023-02-18 02:06:43 +0200

    auth

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
-from src.routes import contacts, users
+from src.routes import contacts, auth
 
 
 app = FastAPI()
 
+app.include_router(auth.router, prefix='/api')
 app.include_router(contacts.router, prefix='/api')
-app.include_router(users.router, prefix='/api')
 
 
 @app.get("/")
