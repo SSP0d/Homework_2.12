b899a409227267d86ee5b444c5e6a57ec9439128
Serhii Pidkopai <spodkopay@gmail.com>
2023-02-18 02:15:46 +0200

    auth

--- src/repository/users.py
+++ src/repository/users.py
@@ -8,6 +8,10 @@ async def get_user_by_username(username: str, db: Session) -> User:
     return db.query(User).filter(User.username == username).first()
 
 
+async def get_user_by_email(email: str, db: Session) -> User:
+    return db.query(User).filter(User.email == email).first()
+
+
 async def create_user(body: UserModel, db: Session) -> User:
     new_user = User(**body.dict())
     db.add(new_user)
--- src/routes/auth.py
+++ src/routes/auth.py
@@ -13,7 +13,7 @@ security = HTTPBearer()
 
 @router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def signup(body: UserModel, db: Session = Depends(get_db)):
-    exist_user = await repository_users.get_user_by_username(body.username, db)
+    exist_user = await repository_users.get_user_by_email(body.email, db)
     if exist_user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account already exists")
     body.password = auth_service.get_password_hash(body.password)
@@ -23,14 +23,14 @@ async def signup(body: UserModel, db: Session = Depends(get_db)):
 
 @router.post("/login", response_model=TokenModel)
 async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = await repository_users.get_user_by_username(body.username, db)
+    user = await repository_users.get_user_by_email(body.email, db)
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
     if not auth_service.verify_password(body.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
     # Generate JWT
-    access_token = await auth_service.create_access_token(data={"sub": user.username})
-    refresh_token = await auth_service.create_refresh_token(data={"sub": user.username})
+    access_token = await auth_service.create_access_token(data={"sub": user.email})
+    refresh_token = await auth_service.create_refresh_token(data={"sub": user.email})
     await repository_users.update_token(user, refresh_token, db)
     return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
 
@@ -38,13 +38,13 @@ async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depen
 @router.get('/refresh_token', response_model=TokenModel)
 async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(security), db: Session = Depends(get_db)):
     token = credentials.credentials
-    data = await auth_service.decode_refresh_token(token)
-    user = await repository_users.get_user_by_username(data, db)
+    email = await auth_service.decode_refresh_token(token)
+    user = await repository_users.get_user_by_email(email, db)
     if user.refresh_token != token:
         await repository_users.update_token(user, None, db)
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token")
 
-    access_token = await auth_service.create_access_token(data={"sub": data})
-    refresh_token = await auth_service.create_refresh_token(data={"sub": data})
+    access_token = await auth_service.create_access_token(data={"sub": email})
+    refresh_token = await auth_service.create_refresh_token(data={"sub": email})
     await repository_users.update_token(user, refresh_token, db)
     return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
