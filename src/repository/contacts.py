542a94062fcffe0f573471a2cbafdd5761e7584b
Serhii Pidkopai <spodkopay@gmail.com>
2023-02-24 01:42:49 +0200

    repository contacts fix

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -1,31 +1,32 @@
 from datetime import datetime, timedelta
 
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
-from src.database.models import Contact
+from src.database.models import Contact, User
 from src.schemas import ContactModel
 
 
-async def create_contact(body: ContactModel, db: Session):
-    contact = Contact(**body.dict())
+async def get_contacts(user: User, db: Session):
+    contacts = db.query(Contact).filter(Contact.user_id == user.id).all()
+    return contacts
+
+
+async def get_contact(contact_id: int, user: User, db: Session):
+    contact = db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
+    return contact
+
+
+async def create_contact(body: ContactModel, user: User, db: Session):
+    contact = Contact(**body.dict(), user_id=user.id)
     db.add(contact)
     db.commit()
     db.refresh(contact)
     return contact
 
 
-async def get_contacts(db: Session):
-    contacts = db.query(Contact).all()
-    return contacts
-
-
-async def get_contact(contact_id: int, db: Session):
-    contact = db.query(Contact).filter_by(id=contact_id).first()
-    return contact
-
-
-async def update_contact(body: ContactModel, contact_id: int, db: Session):
-    contact = db.query(Contact).filter_by(id=contact_id).first()
+async def update_contact(body: ContactModel, contact_id: int, user: User, db: Session):
+    contact = db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
     if contact:
         contact.name = body.name
         contact.surname = body.surname
@@ -37,8 +38,8 @@ async def update_contact(body: ContactModel, contact_id: int, db: Session):
     return contact
 
 
-async def remove_contact(contact_id: int, db: Session):
-    contact = db.query(Contact).filter_by(id=contact_id).first()
+async def remove_contact(contact_id: int, user: User, db: Session):
+    contact = db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
     if contact:
         db.delete(contact)
         db.commit()
@@ -54,6 +55,8 @@ async def searcher(field: str, db: Session):
         if field.lower() in contact.surname.lower() and contact not in contact_list:
             contact_list.append(contact)
         if field.lower() in contact.email.lower() and contact not in contact_list:
+            contact_list.append(contact)
+        if field.lower() in contact.phone and contact not in contact_list:
             contact_list.append(contact)
 
     return contact_list
