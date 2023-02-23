4be882f7855aea109051bf5e2ca9bc2c991e4e06
Serhii Pidkopai <spodkopay@gmail.com>
2023-02-24 01:15:01 +0200

    database modeks fix

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text, func
+from sqlalchemy import Column, Integer, String, DateTime, Text, func, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -9,17 +9,19 @@ class Contact(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False, )
     surname = Column(String(50), nullable=False)
-    email = Column(String(250), unique=True, nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
     phone = Column(String(20), unique=True, nullable=False)
     birthday = Column(DateTime, nullable=False)
+    created_at = Column('created_at', DateTime, default=func.now())
     description = Column(Text, nullable=True)
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
 
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String(50))
-    email = Column(String(250), nullable=False, unique=True)
+    email = Column(String(255), nullable=False, unique=True)
     password = Column(String(255), nullable=False)
     created_at = Column('crated_at', DateTime, default=func.now())
     refresh_token = Column(String(255), nullable=True)
